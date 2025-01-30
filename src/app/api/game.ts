068ad113c6b36f95b91e75c32475e5f23ec557e3
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2025-01-30 09:18:11 -0500

    Make game creation api route.

--- src/app/api/game.ts
+++ src/app/api/game.ts
@@ -1,5 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import Game from "@/utils/models/game";
+import { connectDB } from "@/utils/db";
 
-const createNewGame = () => {
-    const newGame = new Game({})
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Make sure user creates the game with a post request, not visiting the route
+    if (req.method !== "POST")
+        return res.status(405).json({ message: "Method Not Allowed" });
+
+    // Wait untill mongoDB is connected
+    await connectDB();
+
+    // Create the game
+    const newGame = new Game({ gameState: {}, players: [] });
+    await newGame.save();
+
+    // Send gameId to frontend
+    res.status(201).json({ gameId: newGame._id });
 }

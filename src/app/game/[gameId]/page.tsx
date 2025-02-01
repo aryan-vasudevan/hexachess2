e2a9b87d85db9cce1340bdce15d084ab18f3c033
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2025-02-01 14:57:11 -0500

    Seperate home page and game page.

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,14 +1,30 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import axios from "axios";
 import Board from "@/components/board";
+import { Button } from "@mui/material";
 
 export default function GamePage() {
-    const { gameId } = useParams(); 
+    const params = useParams();
+
+    // Parameters might be a string or array
+    const gameId = Array.isArray(params?.gameId)
+        ? params.gameId[0]
+        : params?.gameId;
+
     return (
-        <div>
-            <Board gameId={`${gameId}`}></Board>
+        <div className="my-[100px] place-items-center">
+            {gameId && <Board gameId={gameId} />}
+            <Button
+                variant="contained"
+                onClick={async () => {
+                    await navigator.clipboard.writeText(
+                        `localhost:3000/game/${gameId}`
+                    );
+                }}
+            >
+                Copy Game Link
+            </Button>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import Board from "../components/board";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { Button } from "@mui/material";
 
 export default function Home() {
-    const [gameId, setGameId] = useState<string>("");
+    // Router
+    const router = useRouter(); 
+    const [loading, setLoading] = useState(false);
 
     const createGame = async () => {
+        setLoading(true);
         const res = await axios.post("/api/createGame");
 
-        setGameId(res.data.gameId);
+        // Redirect to the new game page after game creation
+        router.push(`/game/${res.data.gameId}`);
     };
 
-    // Empty dependency array ensures it only runs once when the component is mounted
-    useEffect(() => {
-        createGame();
-    }, []);
-
     return (
-        <div className="my-[100px] place-items-center">
-            {/* Render Board only when gameId is available */}
-            {gameId && <Board gameId={gameId} />}{" "}
-            <Button
-                variant="contained"
-                onClick={async () => {
-                    await navigator.clipboard.writeText(
-                        `localhost:3000/game/${gameId}`
-                    );
-                }}
-            >
-                Copy Game Link
+        <div>
+            <Button variant="contained" onClick={createGame} disabled={loading}>
+                {loading ? "Creating..." : "Create Game"}
             </Button>
         </div>
     );
